7d6addabe7287bd5c3204160a9d3f6de1139d248
Daniel <danie.p.usb@gmail.com>
2023-11-24 13:00:08 +0100

    refactor: hexagonal architecture, create Entity Prices and H2 configuration

--- src/main/java/com/pdaniel/springbootcapitolechallenge/SpringbootCapitoleChallengeApplication.java
+++ src/main/java/com/pdaniel/springbootcapitolechallenge/SpringbootCapitoleChallengeApplication.java
@@ -2,12 +2,12 @@ package com.pdaniel.springbootcapitolechallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.pdaniel.springbootcapitolechallenge")
 public class SpringbootCapitoleChallengeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCapitoleChallengeApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/pdaniel/springbootcapitolechallenge/domain/model/Prices.java
@@ -0,0 +1,98 @@
+package com.pdaniel.springbootcapitolechallenge.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+public class Prices {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long brandId;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private Integer priceList;
+    private Long productId;
+    private Integer priority;
+    private BigDecimal price;
+    private String currency;
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(Long brandId) {
+        this.brandId = brandId;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public Integer getPriceList() {
+        return priceList;
+    }
+
+    public void setPriceList(Integer priceList) {
+        this.priceList = priceList;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}

851c9c0ea0d15ecb7b69caa8fb1d3ccd4efb7e52
Daniel <danie.p.usb@gmail.com>
2023-11-24 01:53:59 +0100

    docs: Initial project

---
+++ src/main/java/com/pdaniel/springbootcapitolechallenge/SpringbootCapitoleChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.pdaniel.springbootcapitolechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCapitoleChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCapitoleChallengeApplication.class, args);
+	}
+
+}

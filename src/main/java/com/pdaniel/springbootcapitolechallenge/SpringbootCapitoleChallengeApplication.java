2f1522dea1301d8ff319c7e1fe3d0b1e708a0a36
Daniel <danie.p.usb@gmail.com>
2023-11-26 18:38:14 +0100

    refactor: Configure Entity and Persistence

--- src/main/java/com/pdaniel/springbootcapitolechallenge/SpringbootCapitoleChallengeApplication.java
+++ src/main/java/com/pdaniel/springbootcapitolechallenge/SpringbootCapitoleChallengeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.pdaniel.springbootcapitolechallenge")
+@EntityScan(basePackages = "com.pdaniel.springbootcapitolechallenge.domain.model")
 public class SpringbootCapitoleChallengeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCapitoleChallengeApplication.class, args);
--- src/main/java/com/pdaniel/springbootcapitolechallenge/domain/model/Price.java
+++ src/main/java/com/pdaniel/springbootcapitolechallenge/domain/model/Price.java
@@ -1,14 +1,12 @@
 package com.pdaniel.springbootcapitolechallenge.domain.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
-public class Prices {
+@Table(name = "price")
+public class Price {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
